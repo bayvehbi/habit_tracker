b87f8d22259b6001061b670c9bf3f6b8beb64278
Vehbi baycan <vehbibaycan@gmail.com>
2026-03-16 11:29:12 +0300

    Initial Commit

---
+++ habit_track/habit_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackApp.swift
+//  habit_track
+//
+//  Created by Vehbi baycan on 16/3/26.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ habit_trackTests/habit_trackTests.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackTests.swift
+//  habit_trackTests
+//
+//  Created by Vehbi baycan on 16/3/26.
+//
+
+import Testing
+@testable import habit_track
+
+struct habit_trackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
